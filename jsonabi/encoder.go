0c3c4014b83164cd20c5f9d3f5846fe07473ee80
Howard Yeh <howard@metacircus.com>
2017-09-29 19:07:38 +0800

    json abi encoder

---
+++ jsonabi/encoder.go
@@ -0,0 +1,141 @@
+package jsonabi
+
+import (
+	"encoding/json"
+	"math/big"
+
+	"github.com/pkg/errors"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+type Encoder struct {
+	abi *abi.ABI
+}
+
+func (e *Encoder) EncodeJSONValues(methodName string, jsonValues []byte) ([]byte, error) {
+	var vals []interface{}
+	err := json.Unmarshal(jsonValues, &vals)
+	if err != nil {
+		return nil, err
+	}
+
+	return e.encodeValues(methodName, vals...)
+}
+
+// todo JSON arrays
+// todo Map as format specifier
+func (e *Encoder) encodeValues(methodName string, vals ...interface{}) ([]byte, error) {
+	method, ok := e.abi.Methods[methodName]
+	if !ok {
+		return nil, errors.Errorf("Cannot find method: %s", methodName)
+	}
+
+	if len(method.Inputs) != len(vals) {
+		return nil, errors.Errorf("Expected %d arguments, got %d", len(method.Inputs), len(vals))
+	}
+
+	var vals2 []interface{}
+	// massage the input arguments
+	for i, arg := range method.Inputs {
+		val := vals[i]
+		t := arg.Type
+		switch t.T {
+		case abi.IntTy:
+			// string or float
+			switch val := val.(type) {
+			case float64:
+				bf := big.NewFloat(val)
+
+				i, err := bigfloatToInty(bf, t.Size)
+				if err != nil {
+					return nil, err
+				}
+				vals2 = append(vals2, i)
+			case string:
+				i, err := stringToInty(val, t.Size)
+				if err != nil {
+					return nil, err
+				}
+				vals2 = append(vals2, i)
+			default:
+				return nil, errors.Errorf("Expected integer got: %v", val)
+			}
+
+		default:
+			vals2 = append(vals2, val)
+		}
+	}
+
+	return e.abi.Pack(methodName, vals2...)
+}
+
+func stringToInty(s string, size int) (interface{}, error) {
+	var i big.Int
+
+	_, ok := i.SetString(s, 0)
+	if !ok {
+		return nil, errors.Errorf("Cannot convert to integer: %s", s)
+	}
+
+	return bigIntToInty(&i, size), nil
+}
+
+// TODO check truncation
+func bigIntToInty(i *big.Int, size int) interface{} {
+	// go-ethereum's ABI encoding is picky about the exact type of a integer. Make it happy.
+	if size == 8 {
+		i := i.Int64()
+		return int8(i)
+	}
+
+	if size == 16 {
+		i := i.Int64()
+		return int16(i)
+	}
+
+	if size == 32 {
+		i := i.Int64()
+		return int32(i)
+	}
+
+	if size == 64 {
+		i := i.Int64()
+		return i
+	}
+
+	// For any other sizes use big.Int
+	return &i
+}
+
+func bigfloatToInty(f *big.Float, size int) (interface{}, error) {
+	// TODO check truncation
+	if !f.IsInt() {
+		return nil, errors.Errorf("Expected integer got float: %s", f.String())
+	}
+
+	// go-ethereum's ABI encoding is picky about the exact type of a integer. Make it happy.
+	if size == 8 {
+		i, _ := f.Int64()
+		return int8(i), nil
+	}
+
+	if size == 16 {
+		i, _ := f.Int64()
+		return int16(i), nil
+	}
+
+	if size == 32 {
+		i, _ := f.Int64()
+		return int32(i), nil
+	}
+
+	if size == 64 {
+		i, _ := f.Int64()
+		return i, nil
+	}
+
+	// For any other sizes use big.Int
+	bigI, _ := f.Int(nil)
+	return bigI, nil
+}
---
+++ jsonabi/encoder_test.go
@@ -0,0 +1,111 @@
+package jsonabi
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+func testMethodABI(methodName, typ string) *abi.ABI {
+	const abiTemplateString = `
+	[{
+		"name": "{{.MethodName}}",
+		"type": "function",
+		"payable": false,
+		"inputs": [
+			{
+				"name": "_a",
+				"type": "{{.Type}}"
+			}
+		],
+		"outputs": [],
+		"constant": false
+	}]
+	`
+
+	abiTemplate := template.Must(template.New("testABI").Parse(abiTemplateString))
+
+	type ctx struct {
+		MethodName string
+		Type       string
+	}
+
+	var abiJSON bytes.Buffer
+	err := abiTemplate.Execute(&abiJSON, &ctx{
+		MethodName: methodName,
+		Type:       typ,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	testABI, err := abi.JSON(&abiJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	return &testABI
+}
+
+func TestEncodeInty(t *testing.T) {
+	is := assert.New(t)
+
+	examples := []struct {
+		argType   string
+		val       string
+		errString string
+	}{
+		{"int8", `[1]`, ""},
+		{"int16", `[1]`, ""},
+		{"int24", `[1]`, ""},
+		{"int32", `[1]`, ""},
+		{"int40", `[1]`, ""},
+		{"int64", `[1]`, ""},
+		{"int72", `[1]`, ""},
+		{"int128", `[1]`, ""},
+		{"int256", `[1]`, ""},
+
+		{"int256", `[1.0]`, ""},
+
+		{"int32", `["0xffff"]`, ""},
+		{"int64", `["0xffff"]`, ""},
+		{"int72", `["0xffff"]`, ""},
+		{"int160", `["0xffff"]`, ""},
+		{"int256", `["0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"]`, ""},
+
+		{"int256", `[1.21]`, "Expected integer got float"},
+		{"int32", `[1.21]`, "Expected integer got float"},
+		{"int32", `[[1,2]]`, "Expected integer got"},
+		{"int32", `[null]`, "Expected integer got"},
+	}
+
+	for _, e := range examples {
+		methodName := "testMethod"
+		testABI := testMethodABI(methodName, e.argType)
+
+		enc := Encoder{
+			abi: testABI,
+		}
+
+		_, err := enc.EncodeJSONValues(methodName, []byte(e.val))
+
+		if e.errString != "" {
+			is.True(strings.HasPrefix(err.Error(), e.errString),
+				fmt.Sprintf("Error message should have prefix: %#v\n\tGot: %#v", e.errString, err.Error()))
+			// is.Contains(err, e.errString)
+			// is.EqualError(err, e.errString)
+		} else {
+			is.NoError(err)
+		}
+
+		// fmt.Println("data", hex.EncodeToString(data))
+
+	}
+}
