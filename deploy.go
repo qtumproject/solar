f65e6147d41de12d9c3b63ec17005970747ad6d9
Howard Yeh <howard@metacircus.com>
2017-12-06 17:54:42 +0800

    specify deploy name with the : separator

--- deploy.go
+++ deploy.go
@@ -4,10 +4,33 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
 )
+
+type deployTarget struct {
+	file string
+	name string
+}
+
+func parseDeployTarget(target string) deployTarget {
+	parts := strings.Split(target, ":")
+
+	if len(parts) == 1 {
+		return deployTarget{
+			file: parts[0],
+			name: parts[0],
+		}
+	}
+
+	// deploy name by default is the file path relative to project root
+	return deployTarget{
+		file: parts[0],
+		name: parts[1],
+	}
+}
 
 func init() {
 	cmd := app.Command("deploy", "Compile Solidity contracts.")
@@ -16,17 +39,19 @@ func init() {
 	noconfirm := cmd.Flag("no-confirm", "Don't wait for network to confirm deploy").Bool()
 	noFastConfirm := cmd.Flag("no-fast-confirm", "(dev) Don't generate block to confirm deploy immediately").Bool()
 
-	sourceFilePath := cmd.Arg("source", "Solidity contracts to deploy.").Required().String()
-	name := cmd.Arg("name", "Name of contract").Required().String()
+	target := cmd.Arg("target", "Solidity contracts to deploy.").Required().String()
+	// name := cmd.Arg("name", "Name of contract").Required().String()
 	jsonParams := cmd.Arg("jsonParams", "Constructor params as a json array").Default("").String()
 
 	appTasks["deploy"] = func() (err error) {
+		target := parseDeployTarget(*target)
+
 		opts, err := solar.SolcOptions()
 		if err != nil {
 			return
 		}
 
-		filename := *sourceFilePath
+		filename := target.file
 
 		compiler := Compiler{
 			Opts:     *opts,
@@ -49,9 +74,9 @@ func init() {
 			repo: repo,
 		}
 
-		fmt.Printf("   \033[36mdeploy\033[0m %s => %s\n", *sourceFilePath, *name)
+		fmt.Printf("   \033[36mdeploy\033[0m %s => %s\n", target.file, target.name)
 
-		err = deployer.CreateContract(*name, *force)
+		err = deployer.CreateContract(target.name, *force)
 		if err != nil {
 			fmt.Println("\u2757\ufe0f \033[36mdeploy\033[0m", err)
 			return
